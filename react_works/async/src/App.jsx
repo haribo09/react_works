0751532c30b06c4064ff394914efa22ab76763b3
haribo09 <das.haribo.1922@gmail.com>
2026-06-20 15:21:48 +0900

    async 수업중

---
+++ react_works/async/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section className='app'>
+        <h2>리엑트 비동기 처리</h2>
+      </section>
+    </>
+  )
+}
+
+export default App
---
+++ react_works/async/src/js_async/async01.js
@@ -0,0 +1,22 @@
+
+function displayA() {
+    console.log('A');
+    
+}
+
+function displayB() {
+    // console.log('B');
+    // 2초 후에 'B' 를 출력
+    setTimeout(() => {
+        console.log('B');
+    }, 2000);
+}
+
+function displayC() {
+    console.log('C');
+    
+}
+
+displayA();
+displayB();
+displayC();
